7de249aa9bd296bf6a3802e86c9acd5ae0946730
paivadul <dul.paiva.comunicaciones@gmail.com>
2024-02-27 00:37:20 -0300

    predecir la desestructuracións

---
+++ script.js
@@ -0,0 +1,7 @@
+const cars = ['Tesla', 'Mercedes', 'Honda'] //3 variables en un array
+const [ randomCar ] = cars  //Tesla
+const [ ,otherRandomCar ] = cars  //Mercedes
+//Predict the output
+console.log(randomCar)  //Tesla
+console.log(otherRandomCar)  //Mercedes
+
